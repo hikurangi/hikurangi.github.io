d279e90f4fd510cb13479b31940d66c656f52cef
hikurangi <hikurangi@gmail.com>
2016-12-10 01:41:33 +1300

    get each blog post to render depending on which page we're on

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,12 +1,10 @@
 import React from 'react'
 
-// Blog may need to accept a JSX string for each blog entry
-// When on the blog route, the URL and the
-
 const Blog = (props) => {
   return (
     <article className="body-copy">
-      { props.html }
+      {/*need to match window.location.pathname to */}
+      { <h1> TESTING </h1> }
     </article>
   )
 }
