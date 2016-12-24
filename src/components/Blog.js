44b954fa57d1c403b180dc7e67102a2836c93bd7
hikurangi <hikurangi@gmail.com>
2016-12-24 17:46:52 +1300

    comments

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,14 +2,17 @@ import React from 'react'
 
 const Blog = (props) => {
 
+  // helper function
   const endsWith = (str, suffix) => {
     return str.indexOf(suffix, str.length - suffix.length) !== -1
   }
 
+  // using the helper function above, find the current entry which has a slug property which ends with the part matching the current url slug
   const currentEntry = props.blog.filter(item => {
     return endsWith(window.location.pathname, item.slug)
   })
 
+  // dangerouslySetInnerHTML with the content of the currentEntry's bodyHTML property.
   const createMarkup = () => {
     return {__html: currentEntry[0].bodyHTML}
   }
