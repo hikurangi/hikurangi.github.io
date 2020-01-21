2c18035f30428c61dfbf96b300b25b6c1a03ea29
Hikurangi Schaverien-Kaa <hikurangi@gmail.com>
2020-01-21 14:53:04 +1300

    Updates

---
+++ precache-manifest.dec0dc493beb3758dea51c42c8e546aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f19ede9cabb5ca204bba35a2774b38d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "874641d031cc7d0f40c8",
+    "url": "/static/css/main.56196714.chunk.css"
+  },
+  {
+    "revision": "fbe19d3e2feaec5e2c1f",
+    "url": "/static/js/2.31dbf67d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.31dbf67d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "874641d031cc7d0f40c8",
+    "url": "/static/js/main.80435153.chunk.js"
+  },
+  {
+    "revision": "f7a65d5cd4551d797616",
+    "url": "/static/js/runtime-main.835801be.js"
+  }
+]);
