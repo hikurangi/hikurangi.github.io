76418846afdad42f51608539840285ca7f7e5515
Hikurangi Schaverien-Kaa <hikurangi@gmail.com>
2020-01-22 15:15:40 +1300

    add global css, update CSP

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,14 @@ module.exports = {
             presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
-      }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
     ]
   }
 }
