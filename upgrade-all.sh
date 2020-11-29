9e0ca57257189d82ce7cc56bd4e272e02d7290ed
Hikurangi <hikurangi@gmail.com>
2020-11-30 11:10:29 +1300

    Upgrade dependencies

---
+++ upgrade-all.sh
@@ -0,0 +1,12 @@
+npm i -D @babel/core@latest \
+@babel/preset-env@latest \
+@babel/preset-react@latest \
+babel-loader@latest \
+clean-webpack-plugin@latest \
+css-loader@latest \
+gh-pages@latest \
+html-webpack-plugin@latest \
+style-loader@latest \
+webpack@latest \
+webpack-cli@latest \
+webpack-dev-server@latest
