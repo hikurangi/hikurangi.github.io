59d4af8d78a190b643bc88cb38cff983306f9e7a
Hikurangi <hikurangi@gmail.com>
2021-06-08 00:21:32 +1200

    Refactor component definition

--- src/components/Content.js
+++ src/components/Content.js
@@ -11,24 +11,24 @@ const styles = {
 const Text = () =>
   p({ style: styles.mainText }, [
     'Hikurangi Schaverien-Kaa ',
-    makeLink('https://nzmusician.co.nz/features/,get-yer-kit-off-hikurangi-schaverien-kaa/', 'beats the heck out of the drums'),
+    Link('https://nzmusician.co.nz/features/,get-yer-kit-off-hikurangi-schaverien-kaa/', 'beats the heck out of the drums'),
     ' and ',
-    makeLink('https://github.com/hikurangi', 'writes clean code'),
+    Link('https://github.com/hikurangi', 'writes clean code'),
     '. He\'s mostly based in Wellington, NZ. You can reach him via ',
-    makeLink(`mailto:${email}`, em(email)),
+    Link(`mailto:${email}`, em(email)),
     ' or find him ',
-    makeLink('https://instagram.com/ignarukih', 'on'),
+    Link('https://instagram.com/ignarukih', 'on'),
     ' ',
-    makeLink('https://soundcloud.com/ignarukih', 'the'),
+    Link('https://soundcloud.com/ignarukih', 'the'),
     ' ',
-    makeLink('https://www.linkedin.com/in/ignarukih/', 'socials'),
+    Link('https://www.linkedin.com/in/ignarukih/', 'socials'),
     '.'
   ])
 
 export default Text
 
-function makeLink (href, content) { return a({
+const Link = (href, content) => a({
   href,
   target: '_blank',
   rel: 'noopener noreferrer'
-}, [content]) }
+}, [content])
