9892a42f78c5df1823e43544250f8d9be71a43ba
Hikurangi <hikurangi@gmail.com>
2021-06-07 15:27:09 +1200

    Add assets

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 echo 'Building...\n' && \
 NODE_ENV='production' npm run build && \
+echo '\nSupplying assets...\n' && \
+cp ./assets/* ./build/ &&
 echo '\nCreating CNAME record...\n' && \
 echo 'hikurangi.co' > build/CNAME
